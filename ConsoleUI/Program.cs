1fa38f18e1acba18126eb1b053c6f53432748034
Aleksandar Djurdjević <aleksa.djdj@gmail.com>
2019-01-14 22:18:29 +0100

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
+        }
+    }
+}
---
+++ DemoLibrary/Utilities/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DemoLibrary.Utilities
+{
+    public class DataAccess : IDataAccess
+    {
+        public void SaveData(string name)
+        {
+            Console.WriteLine($"Saving { name }");
+        }
+
+        public void LoadData()
+        {
+            Console.WriteLine("Loading Data");
+        }
+    }
+}
